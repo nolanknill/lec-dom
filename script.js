3481cca85a8571c622d4334aa36cbd5d5e11e6d6
Nolan Knill <nolanknill@gmail.com>
2023-07-10 12:48:19 -0400

    Add tv shows array

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Now we can use the DOM to output HTML!")
+const tvShows = [
+    {
+        title: "Suits",
+        "number-of-episodes": 100,
+        genres: ["drama", "legal"],
+        numberOfSeasons: 10,
+        castMembers: ["Gabriel Macht", "Patrick J. Adams", "Meghan, Duchess of Sussex"],
+        year: 2011,
+    },
+    {
+        title: "Game of Thrones",
+        "number-of-episodes": 60,
+        genres: ["fantasy", "disappointing finish"],
+        numberOfSeasons: 8,
+        castMembers: ["Thor Bjornsson", "Kit Harrington",  "Emilia Clarke"],
+        year: 2011,
+        rating: 2
+    },
+    {
+        title: "Teletubbies",
+        "number-of-episodes": 365,
+        genres: ["educational", "children"],
+        numberOfSeasons: 2,
+        castMembers: ["Red", "Yellow",  "Green", "Purple"],
+        year: 2001,
+        rating: 8.91
+    }
+]
