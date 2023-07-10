f12acf119ce54a609cf703db4ed38719e3aca9a2
Nolan Knill <nolanknill@gmail.com>
2023-07-10 16:32:06 -0400

    Uses DOM to add, remove DOM method items. Adds TV shows

--- script.js
+++ script.js
@@ -1,1 +1,74 @@
 console.log("Now we can use the DOM to output HTML!");
+
+const titleEl = document.querySelector(".header__page-title");
+
+titleEl.innerText = "Best lecture this week";    
+
+const listItemEls = document.querySelectorAll("li");
+
+/*
+for (let i = 0; i < listItemEls.length; i++) {
+    const listItemEl = listItemEls[i];
+    listItemEl.innerText = "You've been hacked lol";
+}
+*/
+
+const listItemEl = listItemEls[listItemEls.length - 1];
+listItemEl.innerText = "You've been hacked lol";
+listItemEl.classList.add("dom-methods__item--hacked")
+listItemEl.classList.remove("dom-methods__item--hacked");
+listItemEl.classList.toggle("dom-methods__item--hacked");
+
+
+/* Add a list item : lorem ipsum  */
+/* 
+    <li class="dom-methods__item">Lorem Ipsum</li>
+*/
+/*
+1. grab the parent element : ulEl = document.querySelector(".dom-methods");
+2. create the LI element (child), add text, add class
+    let liEl = document.createElement("li")
+    liEl.innerText = "Lorem Ipsum";
+    liEl.classList.add("dom-methods__item")
+3. append child to parent :
+    ulEl.appendChild(liEl);
+*/
+
+const domMethodsEl = document.querySelector(".dom-methods");
+
+const newMethodEl = document.createElement("li");
+newMethodEl.innerText = "Lorem Ipsum";
+newMethodEl.classList.add("dom-methods__item");
+
+domMethodsEl.appendChild(newMethodEl);
+
+
+// const sports = null / undefined / 0;
+// falsy values = "", undefined, null, NaN, false, 0, -0
+
+// if ( sports ) {
+//     alert("hey");
+// }
+
+
+/**
+ * 3 options for removing HTML elements:
+ *      1. clear innerHTML
+ *      2. reverse for loop with removeChild
+ *      3. while loop with firstChild, removeChild, lastChild
+ */
+
+/* Option 1: clearing innerHTML */
+// domMethodsEl.innerHTML = "";
+
+/* Option 2: reverse for loop with removeChild */
+const itemsEl = document.querySelectorAll(".dom-methods__item");
+for (let i = itemsEl.length - 1; i >= 0; i--) {
+    domMethodsEl.removeChild(itemsEl[i])
+}
+
+/* Option 3: while loop with firstChild / removeChild / lastChild
+while (domMethodsEl.firstChild) {
+    domMethodsEl.removeChild(domMethodsEl.lastChild);
+}
+*/
--- tv-shows.js
+++ tv-shows.js
@@ -28,5 +28,49 @@ const tvShows = [
         year: 2001,
         rating: 8.91,
         imageSrc: "/assets/images/teletubbies.jpeg"
+    },
+    {
+        title: "Rick & Morty",
+        rating: 10,
+        imageSrc: "/assets/images/rick-and-morty.png"
     }
 ]
+
+const tvShowsEl = document.querySelector(".tv-shows");
+
+for (let i = 0; i < tvShows.length; i++) {
+    const articleEl = createTvShowEl(tvShows[i]);
+    tvShowsEl.appendChild(articleEl);
+}
+
+/*
+    <article class="tv-show">
+        <h2 class="tv-show__title">${tvShow.title}</h2>
+        <span class="tv-show__rating">${tvShow.rating}</span>
+        <img class="tv-show__cover" src="${tvShow.imageSrc}" alt="${tvShow.title} Cover" /> 
+    </article>
+*/
+function createTvShowEl(tvShow) {
+    const tvShowEl = document.createElement("article");
+    tvShowEl.classList.add("tv-show");
+
+    const titleEl = document.createElement("h2");
+    titleEl.innerText = tvShow.title;
+    titleEl.classList.add("tv-show__title");
+
+    const ratingEl = document.createElement("span");
+    ratingEl.innerText = tvShow.rating;
+    ratingEl.classList.add("tv-show__rating");
+    
+    const coverEl = document.createElement("img");
+    coverEl.classList.add("tv-show__cover");
+    coverEl.setAttribute("alt", tvShow.title + " Cover");
+    coverEl.src = tvShow.imageSrc;
+    // coverEl.setAttribute("src", tvShow.imageSrc);
+    
+    tvShowEl.appendChild(titleEl);
+    tvShowEl.appendChild(ratingEl);
+    tvShowEl.appendChild(coverEl);
+
+    return tvShowEl;
+}
