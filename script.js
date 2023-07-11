128c787ec5d6660790041e222531b81db5f72591
Nolan Knill <nolanknill@gmail.com>
2023-07-11 12:23:29 -0400

    Adds demo code for DOM. Place all TV shows from an array into the DOM

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-console.log("Now we can use the DOM to output HTML!");
+// TODO: Modify text and styles for all items in list
+/* For loop,  
+let domMethodEls = document.querySelectorAll(".dom-methods__item");
+
+for (let i = 0; i < domMethodEls.length; i++) {
+    let currentEl = domMethodEls[i];
+
+    currentEl.innerText = "You've been hacked lolz";
+    currentEl.style.color = "greenyellow";
+    currentEl.style.backgroundColor = "black";
+    currentEl.style.fontSize = "3rem";
+}
+*/
+let domMethodEls = document.querySelectorAll(".dom-methods__item");
+
+let lastDomMethodEl = domMethodEls[domMethodEls.length - 1];
+
+lastDomMethodEl.innerText = "You've been hacked lolz";
+
+lastDomMethodEl.classList.add("dom-methods__item--hacked");
+/*
+lastDomMethodEl.style.color = "greenyellow";
+lastDomMethodEl.style.backgroundColor = "black";
+lastDomMethodEl.style.fontSize = "3rem";
+*/
+lastDomMethodEl.classList.remove("dom-methods__item--hacked");
+lastDomMethodEl.classList.toggle("dom-methods__item--hacked");
+
+
+/* 
+    TODO: add a list item to our list
+    
+    <li class="dom-methods__item">document.createElement()</li>
+
+    1. Grab the parent element 
+    2. Create the child element (document.createElement + anything else)
+    3. Append child element to parent (parent.appendChild(child))
+*/
+
+const listEl = document.getElementById("dom-methods");
+
+const itemEl = document.createElement("li");
+itemEl.classList.add("dom-methods__item");
+itemEl.innerText = "document.createElement()";
+
+listEl.appendChild(itemEl);
+
+listEl.innerHTML = "";
--- tv-shows.js
+++ tv-shows.js
@@ -1,32 +1,79 @@
 const tvShows = [
     {
-        title: "Suits",
-        "number-of-episodes": 100,
-        genres: ["drama", "legal"],
-        numberOfSeasons: 10,
-        castMembers: ["Gabriel Macht", "Patrick J. Adams", "Meghan, Duchess of Sussex"],
-        year: 2011,
-        rating: 8,
-        imageSrc: "/assets/images/suits.jpg"
+        title: "Breaking Bad",
+        genres: ["Drama", "Thriller"],
+        cast: ["Aaron Paul", "Bryan Cranston"],
+        "theme-song": "Let Them Cook",
+        releaseYear: 2008,
+        runLength: 45,
+        rating: 10,
+        imageSrc: "/assets/images/breaking-bad.jpeg"
     },
     {
-        title: "Game of Thrones",
-        "number-of-episodes": 60,
-        genres: ["fantasy", "disappointing finish"],
-        numberOfSeasons: 8,
-        castMembers: ["Thor Bjornsson", "Kit Harrington",  "Emilia Clarke"],
-        year: 2011,
-        rating: 2,
-        imageSrc: "/assets/images/game-of-thrones.jpeg"
+        title: "House of the Dragon",
+        genres: ["Fantasy", "Drama"],
+        cast: ["Milly Alcock", "Emma D'Arcy", "Matt Smith"],
+        "theme-song": "Game of Thrones (Original)",
+        releaseYear: 2022,
+        runLength: 50,
+        rating: 10,
+        imageSrc: "/assets/images/house-of-the-dragon.jpeg"
     },
     {
-        title: "Teletubbies",
-        "number-of-episodes": 365,
-        genres: ["educational", "children"],
-        numberOfSeasons: 2,
-        castMembers: ["Red", "Yellow",  "Green", "Purple"],
-        year: 2001,
-        rating: 8.91,
-        imageSrc: "/assets/images/teletubbies.jpeg"
-    }
-]
+        title: "Squid Game",
+        genres: ["Thriller", "Horror"],
+        cast: ["HoYeon Jung", "Lee Jung-jae", "Gong Yoo"],
+        "theme-song": "Squid Games (Original)",
+        releaseYear: 2021,
+        runLength: 50,
+        rating: 10,
+        imageSrc: "/assets/images/squid-game.jpeg"
+    },
+    {
+        title: "True Detective",
+        rating: 21,
+        imageSrc: "/assets/images/true-detective.png"
+    },
+];
+
+/* Add all TV Shows to the tv-shows element */
+const tvShowsEl = document.getElementById("tv-shows");
+for (let i = 0; i <= tvShows.length - 1; i++) {
+    const showEl = createTvShowElement(tvShows[i]);
+    tvShowsEl.appendChild(showEl);
+}
+
+
+/*
+    Input: show will contain { Title, Rating, Image }
+    
+    Output: Element object that looks like:
+    <article class="tv-show">
+        <h2 class="tv-show__title">${show.title}</h2>
+        <span class="tv-show__rating">Rating: ${show.rating}</span>
+        <img class="tv-show__cover" src="${show.imageSrc}" alt="${show.title} Cover" />
+    </article>
+*/
+function createTvShowElement(show) {
+    const showEl = document.createElement("article");
+    showEl.classList.add("tv-show");
+
+    const titleEl = document.createElement("h2");
+    titleEl.classList.add("tv-show__title");
+    titleEl.innerText = show.title;
+
+    const ratingEl = document.createElement("span");
+    ratingEl.classList.add("tv-show__rating");
+    ratingEl.innerText = "Rating: " + show.rating;
+    
+    const coverEl = document.createElement("img");
+    coverEl.classList.add("tv-show__cover");
+    coverEl.setAttribute("src", show.imageSrc);
+    coverEl.setAttribute("alt", show.title + " Cover");
+    
+    showEl.appendChild(titleEl);
+    showEl.appendChild(ratingEl);
+    showEl.appendChild(coverEl);
+
+    return showEl;
+}
